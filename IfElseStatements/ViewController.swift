808e1dfc196dd8a743904fab42e3639ac798ea7c
Jerimie Lucero <jerimie@hawaii.edu>
2019-01-31 10:27:22 -1000

    Initial Commit

---
+++ IfElseStatements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IfElseStatements
+//
+//  Created by Jerimie Lucero on 1/31/19.
+//  Copyright © 2019 Jerimie Lucero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
